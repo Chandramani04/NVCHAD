b3133db07a135cbe5c85ec916a80aae5aad46086
Chandramani04 <chandramani2701@gmail.com>
2024-10-08 22:55:24 +0530

    update

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,21 +1,32 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    css = { "prettier" },
-    html = { "prettier" },
-    javascript = { "prettier" },
-    javascriptreact = { "prettier" },
-    typescript = { "prettier" },
-    typescriptreact = { "prettier" },
-    json = { "prettier" },
-    markdown = { "prettier" },
-  },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        css = { "prettier" },
+        html = { "prettier" },
+        -- javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        javascript = {
+            cmd = "prettier",
+            args = { "--tab-width", "4" },
+            stdin = true,
+        },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        json = { "prettier" },
+        markdown = { "prettier" },
+    },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    lsp_fallback = true,
-    timeout_ms = 500,
-  },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        lsp_fallback = true,
+        timeout_ms = 500,
+    },
+    format = function(bufnr)
+        vim.bo[bufnr].tabstop = 4
+        vim.bo[bufnr].shiftwidth = 4
+        vim.bo[bufnr].expandtab = true -- Converts tabs to spaces
+        return {}
+    end,
 }
 
 return options
